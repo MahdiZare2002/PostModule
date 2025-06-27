98668398e7fd51ea5281f10cd68ea90008714905
Mahdi <mahdi.zed2002@gmail.com>
2025-06-27 18:24:15 +0330

    add city application contracts to Application Contract layer

---
+++ PostModule.Application.Contract/CityApplication/CreateCityModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostModule.Application.Contract.CityApplication
+{
+    public class CreateCityModel
+    {
+        public string Title { get; set; }
+        public int StateId { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ PostModule.Application.Contract/CityApplication/EditCityModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostModule.Application.Contract.CityApplication
+{
+    public class EditCityModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Status { get; set; }
+    }
+}
